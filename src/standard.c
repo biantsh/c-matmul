633f2859d98657099b5c77ab4dc7de82c643e8b0
Biant Shkololli <shkolollibiant@gmail.com>
2023-12-09 22:13:58 +0100

    Get matrix size as CLI arg

--- src/standard.c
+++ src/standard.c
@@ -4,39 +4,40 @@
 #include <stdlib.h>
 #include <time.h>
 
-#define SIZE 512
-
-void mat_mul(int* matrix_1[SIZE], int* matrix_2[SIZE], int* result[SIZE]) {
-    for (int row = 0; row < SIZE; row++) {
-        for (int col = 0; col < SIZE; col++) {
+void mat_mul(int size, int* matrix_1[size], int* matrix_2[size], int* result[size]) {
+    for (int row = 0; row < size; row++) {
+        for (int col = 0; col < size; col++) {
             result[row][col] = 0;
-            for (int index = 0; index < SIZE; index++) {
+            for (int index = 0; index < size; index++) {
                 result[row][col] += matrix_1[row][index] * matrix_2[index][col];
             }
         }
     }
 }
 
-int main() {
-    int* matrix_1[SIZE];
-    int* matrix_2[SIZE];
-    int* result[SIZE];
+int main(int argc, char* argv[]) {
+    long size = strtol(argv[1], NULL, 10);
 
-    for (int row = 0; row < SIZE; row++) {
-        matrix_1[row] = (int *) malloc(SIZE * sizeof(int));
-        matrix_2[row] = (int *) malloc(SIZE * sizeof(int));
-        result[row] = (int *) malloc(SIZE * sizeof(int));
+    int* matrix_1[size];
+    int* matrix_2[size];
+    int* result[size];
 
-        for (int col = 0; col < SIZE; col++) {
+    for (int row = 0; row < size; row++) {
+        matrix_1[row] = (int *) malloc(size * sizeof(int));
+        matrix_2[row] = (int *) malloc(size * sizeof(int));
+        result[row] = (int *) malloc(size * sizeof(int));
+
+        for (int col = 0; col < size; col++) {
             matrix_1[row][col] = rand() % 10;
             matrix_2[row][col] = rand() % 10;
         }
     }
 
     clock_t time_start = clock();
-    mat_mul(matrix_1, matrix_2, result);
+    mat_mul(size, matrix_1, matrix_2, result);
     clock_t time_end = clock();
 
     double time_taken = (double) (time_end - time_start) / CLOCKS_PER_SEC;
     printf("Time taken: %.3f seconds\n", time_taken);
+    printf("Matrix size: %d", size);
 }
