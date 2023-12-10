6d7eb1089e9d3f795cb7ece5fd021af0c34ed9f1
Biant Shkololli <shkolollibiant@gmail.com>
2023-12-10 04:00:57 +0100

    Refactor standard implementation to use functions from src/lib

--- src/standard.c
+++ src/standard.c
@@ -4,6 +4,8 @@
 #include <stdlib.h>
 #include <time.h>
 
+# include "lib/mat_util.h"
+
 void mat_mul(int size, int** matrix_1, int** matrix_2, int** result) {
     for (int row = 0; row < size; row++) {
         for (int col = 0; col < size; col++) {
@@ -18,20 +20,13 @@ void mat_mul(int size, int** matrix_1, int** matrix_2, int** result) {
 int main(int argc, char* argv[]) {
     long size = strtol(argv[1], NULL, 10);
 
-    int* matrix_1[size];
-    int* matrix_2[size];
-    int* result[size];
+    int** matrix_1 = mat_alloc(size);
+    int** matrix_2 = mat_alloc(size);
+    int** result = mat_alloc(size);
 
-    for (int row = 0; row < size; row++) {
-        matrix_1[row] = (int *) malloc(size * sizeof(int));
-        matrix_2[row] = (int *) malloc(size * sizeof(int));
-        result[row] = (int *) malloc(size * sizeof(int));
-
-        for (int col = 0; col < size; col++) {
-            matrix_1[row][col] = rand() % 10;
-            matrix_2[row][col] = rand() % 10;
-        }
-    }
+    mat_fill(matrix_1, size);
+    mat_fill(matrix_2, size);
+    mat_fill(result, size);
 
     clock_t time_start = clock();
     mat_mul(size, matrix_1, matrix_2, result);
